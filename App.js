292a84d4691410577ad716d1f209936950ba1ac7
Federico Peirano <fedepr2345@gmail.com>
2024-04-27 19:21:24 -0300

    c. Comunicandose con el servidor - Ejercicio10.12 - Variables de Entorno

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import Main from './src/screens/MainScreen';
 import { NativeRouter } from 'react-router-native';
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
+import Constants from 'expo-constants';
 
 import createApolloClient from './src/utils/apolloClient';
 
 const apolloClient = createApolloClient();
+const appConfig = Constants.expoConfig;
+
 const App = () => {
+  console.log(appConfig);
+
   return (
     <NativeRouter>
       <ApolloProvider client={apolloClient}>
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import ApolloClient from 'apollo-boost';
+import Constants from 'expo-constants';
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://192.168.1.33:5000//graphql',
+    uri: Constants.expoConfig.extra.env.APOLLO_URI,
   });
 };
 
